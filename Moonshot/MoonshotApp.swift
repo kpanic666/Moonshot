9f8441d81ad051e8c9b9a6f17ae50c14cc7c5112
Andrei Korikov <kpanic666@gmail.com>
2021-08-04 18:01:24 +0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Andrei Korikov on 04.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
